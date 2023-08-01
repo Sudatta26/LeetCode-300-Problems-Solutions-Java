9108b6abef30afbaac5d1fdc2f2dac19487fdbf6
Sudatta Jana <109480484+Sudatta26@users.noreply.github.com>
2023-08-01 11:26:55 +0530

    Time: 1 ms (99.94%), Space: 59.7 MB (18.88%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -17,7 +17,7 @@ class Solution {
       }
     public void swap(int[] arr, int i,int j){
         int temp = arr[i];
-        arr[i]=arr[j];
+        arr[i]=arr[j];//
         arr[j]=temp;
     }  
 }
