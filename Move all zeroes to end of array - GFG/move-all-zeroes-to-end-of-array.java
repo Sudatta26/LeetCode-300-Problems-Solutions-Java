19ff743d7ce2ff2fd80ab470b53d90879d0a98e3
Sudatta Jana <109480484+Sudatta26@users.noreply.github.com>
2023-08-05 20:41:27 +0530

    Added solution - LeetHub

---
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+        // code here
+       int j = 0;
+for (int i = 0; i < arr.length; i++) {
+    if (arr[i] != 0) {
+        arr[j] = arr[i];
+        j++;
+    }
+}
+
+// Fill the rest of the array with zeros
+while (j < arr.length) {
+    arr[j] = 0;
+    j++;
+}
+
+// Now, 'arr' contains the elements from the original array, with zeros at the end.
+    }
+}
