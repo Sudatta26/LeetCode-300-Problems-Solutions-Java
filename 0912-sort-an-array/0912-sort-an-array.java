5f706c7f43afeed096a0b1aaed447609fdea607c
Sudatta Jana <109480484+Sudatta26@users.noreply.github.com>
2024-07-25 18:53:54 +0530

    Time: 19 ms (81.75%), Space: 55.5 MB (78.10%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,14 +1,7 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        for(int i=1;i<nums.length;i++){
-			int curr=nums[i];
-			int j= i-1;
-			while(j>=0 && curr<nums[j]){
-				nums[j+1]=nums[j];
-				j--;
-			}
-			nums[j+1]=curr;
-		}
-		return nums;
+        Arrays.sort(nums);
+        return nums;
+    
     }
 }
